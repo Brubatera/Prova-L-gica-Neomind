7f3db8f032690fae97f256eb525b201eface46ea
Bruno Martendal Prudêncio <31561187+Brubatera@users.noreply.github.com>
2018-12-28 14:31:03 -0200

    Update relogio2.java

--- Calcular relógios/relogio2.java
+++ Calcular relógios/relogio2.java
@@ -8,8 +8,8 @@ public class relogio2{
 	public static void main(String[] args){
 	{
    Scanner ler = new Scanner(System.in);
-   try{
-         int a = 360; //circunferÍncia do relÛgio
+
+         int a = 360; //circunfer√™ncia do rel√≥gio
          int b = 12; //total de horas
          int c = 60; //minutos que equivalem a 1 hora
          System.out.printf("Informe o valor da horas:");
@@ -20,20 +20,18 @@ public class relogio2{
          int ponteiroHora = 0;
          int ponteiroMinuto = 0;
          
-         ponteiroHora =(int)((horas * a) / b + (minutos * 0.5)); //faz o calculo da movimentaÁ„o em graus do ponteiro das horas
-         ponteiroMinuto = (minutos * a) /c; // faz o c·lculo da movimentaÁ„o em graus do ponteiro dos minutos
+         ponteiroHora = (int)((horas * a) / b + (minutos * 0.5)); //faz o c√°lculo da movimenta√ß√£o em graus do ponteiro das horas
+         ponteiroMinuto = (minutos * a) /c; // faz o c√°lculo da movimenta√ß√£o em graus do ponteiro dos minutos
              
-            if(ponteiroHora > ponteiroMinuto){ //para obter o resultado correto, subtraÌmos o maior valor pelo menor
+            if(ponteiroHora > ponteiroMinuto){ //para obter o resultado correto, subtra√≠mos o maior valor pelo menor
                   graus = ponteiroHora - ponteiroMinuto;
              }else{
                   graus = ponteiroMinuto - ponteiroHora;
              }
              
-               System.out.println(graus + " graus");
-               
-   }finally{
+               System.out.println(graus + " graus");              
+
       ler.close();
-   }
-  }
+}
 }
 }
